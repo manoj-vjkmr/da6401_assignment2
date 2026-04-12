707ba59d5af8a16657599f17a6e6c80ffb8d5903
manoj-vjkmr <manojharish565@gmail.com>
2026-04-12 22:50:12 +0530

    a

--- models/multitask.py
+++ models/multitask.py
@@ -32,7 +32,7 @@ class MultiTaskPerceptionModel(nn.Module):
             try:
                 import gdown
                 gdown.download(id="1ErN9X3BrHDFvRTE1nniTRAbRfRIimfPU", output=classifier_path, quiet=False)
-                gdown.download(id="1D4ZYECyqb42wOkW28PkCo2y1h5dyke9L", output=localizer_path, quiet=False)
+                gdown.download(id="1HSqaWzqC2EIx-2N6YHm4LbKnc5Ky4SGv", output=localizer_path, quiet=False)
                 gdown.download(id="1JfScQk7-VKErpVM9pTHxWEB0s_c15ZWC", output=unet_path, quiet=False)
             except Exception as e:
                 print(f"Download failed: {e}")
